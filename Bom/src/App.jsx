a3252d38bde54bd413a345ee10b2313d5ed59d46
Vilped1 <vildeandrea01@gmail.com>
2024-09-20 13:52:45 +0200

    La til første artikkel

---
+++ Bom/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import './style/main.scss'
+import Drinking from './components/Drinking'
+
+function App() {
+
+  return (
+    <>
+      <Drinking/>
+    </>
+  )
+}
+
+export default App
---
+++ Bom/src/components/Drinking.jsx
@@ -0,0 +1,69 @@
+export default function Drinking() {
+    return(
+        <section id="content">
+        <header id="header">
+            <h1 id="title">Drikkingens dilemma: <br/> Hvordan former alkohol studentmiljøet?</h1>                                                                
+            <p class="foto">Foto: Jenny Østreng</p>
+        </header>
+
+        <main>
+            <article>
+                <p class="sitat" id="Jonas">- Jeg synes det er litt leit at det alltid er alkohol med i bildet når man skal finne på noe med andre studenter, sier Jonas Heggelund (24), student ved Høgskolen i Østfold</p>
+                
+                <figure>
+                    <img src="./pictures/man-drink.PNG" alt="kollektiv-bilde" />
+                    <p class="foto-text">
+                        Jonas Heggelund er lei av at studentene alltid drikker alkohol ved sosiale anledninger.
+                    </p>
+                    <figcaption class="foto">
+                        Foto: Jenny Østreng
+                    </figcaption>
+                </figure>
+
+                {/* <div id="man-drink">
+                    <img src="./sources/man-drink.PNG" alt="kollektiv-bilde">
+                    <p class="foto-text">
+                        Jonas Heggelund er lei av at studentene alltid drikker alkohol ved sosiale anledninger.
+                    </p>
+                    <p class="foto">Foto: Jenny Østreng</p>
+                </div> */}
+
+                <h3>Halvparten av studentene mener det drikkes for mye</h3>
+                <p>
+                    Forskning viser at alkoholbruk blant studenter er utbredt, og mange deltar i såkalt «binge drinking» – drikking av fem til seks enheter eller mer i én setting. Samtidig viser SHoTundersøkelsen (Studentenes helse- og trivselsundersøkelse) fra 2022 at halvparten av studentene mener det drikkes for mye. Seks av ti studenter etterlyser flere alkoholfrie tilbud, og to av ti sier at de har unngått studentarrangementer på grunn av alkoholkulturen, og at det drikkes for mye.
+                </p>
+
+                <h4>Fadderuka – sosial velkomst eller drikkepress?</h4>
+                <p>
+                    Fadderuka markerer starten på studietiden for mange nye studenter, og er ment å være en sosial, inkluderende uke med aktiviteter som bygger fellesskap. Målet er å gi studentene en god start og hjelpe dem med å knytte varige vennskap og relasjoner. Likevel opplever mange at fadderuka er preget av sterkt alkoholfokus, noe som skaper press for å delta i festkulturen. For mange fører dette til frykt for å havne utenfor det sosiale miljøet dersom de ikke drikker i like stor grad som andre, og mange ender opp med å drikke alkohol, selv om de ikke vil.
+                </p>
+                <p class="sitat">- Jeg følte på mye press under fadderuka om å alltid ta del i festkulturen. Selv om jeg ikke nødvendigvis ønsket å drikke så veldig mye, følte jeg på at jeg måtte det bare for å passe inn med alle rundt meg, sier Heggelund.</p>
+
+                <h4>En todelt opplevelse</h4>
+                <p class="sitat">- For å være helt ærlig, så er det eneste alternativet til fest å dra på brettspillkveld med boller og brus. Det er liksom ikke like kult.
+                </p>
+
+                <figure>
+                    <img src="./pictures/beerpong.PNG" alt="kollektiv-bilde" />
+                    <p class="foto-text">
+                        Her står studentene samlet rundt bordet for den velkjente drikkeleken "beer pong".
+                    </p>
+                    <figcaption class="foto">
+                        Foto: Jenny Østreng
+                    </figcaption>
+                </figure>
+
+                {/* <div id="beer-pong">
+                    <p class="foto-text">Her står studentene samlet rundt bordet for den velkjente drikkeleken "beer pong".</p>
+                    <p class="foto">Foto: Jenny Østreng</p>
+                </div> */}
+
+                <p>
+                    For noen studenter er alkohol en naturlig del av studentlivet som de tror kan styrke sosiale relasjoner og fellesskap. For andre fungerer alkohol imidlertid som en barriere som skaper følelser av ekskludering. Mange oppfatter også de alkoholfrie alternativene som mindre attraktive, noe som fører til et press om å drikke for å unngå å stå utenfor. Når studentene ble spurt om hvilke alternativer som finnes til alkohol og fest, svarer Ronja Haraldseth (20) dette:
+                </p>
+                <p class="sitat">- Flere studenter ved Høgskolen i Østfold mener derfor at det er på tide å skape et mer inkluderende studentmiljø ved å tilby flere attraktive alkoholfrie alternativer.</p>
+            </article>
+        </main>
+    </section>
+    )
+}
