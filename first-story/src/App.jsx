67e93c65a036ec8dafe6ce90b606ff8e077ba97f
AnneOmberg <111729606+AnneOmberg@users.noreply.github.com>
2024-09-21 21:33:03 +0200

    litt scss

--- first-story/src/App.jsx
+++ first-story/src/App.jsx
@@ -6,7 +6,6 @@ import Content from './components/Content'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- first-story/src/components/Header.jsx
+++ first-story/src/components/Header.jsx
@@ -4,11 +4,16 @@ export default function Header() {
     return (
         <header>
             <section>
-                <h2>Overskrift</h2>
+                <h2 id="header-title">Drikkingens dilemma:
+                    Hvordan former alkohol studentmiljøet?</h2>
             </section>
             <ul>
-                <li>Ida</li>
-                <li>Jenny</li>
+                <li>Journalist & fotograf</li>
+                <strong>
+                    <li>Jenny Østreng</li>
+                    <li>Ida Marie Olsen</li>
+                </strong>
+                <li>Tirsdag 10. september 2024</li>
             </ul>
         </header>
     )
