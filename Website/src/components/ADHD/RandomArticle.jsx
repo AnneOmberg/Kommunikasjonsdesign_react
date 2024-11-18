acdebe01751bd1945ac4c0482104ec7ee6e291c3
Vilped1 <vildeandrea01@gmail.com>
2024-11-18 18:05:38 +0100

    random

--- Website/src/components/ADHD/RandomArticle.jsx
+++ Website/src/components/ADHD/RandomArticle.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { data } from "./data";
 
 export default function RandomArticle() {
     const viewPoint = ["late", "early"];
@@ -30,6 +32,30 @@ export default function RandomArticle() {
                         </section>
                     )}
                 </article>
+            </section>
+            <section id="chart">
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                    >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis yAxisId="left" />
+                    <YAxis yAxisId="right" orientation="right" />
+                    <Tooltip />
+                    <Legend />
+                    <Line yAxisId="left" type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line yAxisId="right" type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    </LineChart>
+                </ResponsiveContainer>
             </section>
         </main>
     )
