a24bd38a288ed93714e0bd9ee5a2dc054037f32c
Vilped1 <vildeandrea01@gmail.com>
2024-11-19 13:09:30 +0100

    Oppg2

--- Website/src/components/ADHD/RandomArticle.jsx
+++ Website/src/components/ADHD/RandomArticle.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, Legend, Rectangle, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 import { data } from "./data";
 
 export default function RandomArticle() {
@@ -20,8 +20,39 @@ export default function RandomArticle() {
 
     return (
         <main id="adhd">
+            <section className="header adhdHeader">
+                <h1 className="title" id="adhdTitle">ADHD-diagnose:<br />Hva betyr tidspunktet?</h1>
+            </section>
             <section id="content">
                 <article>
+                    <p>Mellom 2008 og 2013 var 4,3 % av gutter og 1,7 % av jenter i alderen 6–17 år registrert med en ADHD-diagnose i Norsk pasientregister, ifølge en rapport fra Folkehelseinstituttet (FHI) fra 2016. De fleste får diagnosen tidlig i skolealder, og FHI beskriver tidlig intervensjon som avgjørende for å gi barn bedre forutsetninger i livet. Likevel er det mange barn som ikke fanges opp av spesialisthelsetjenesten, noe som bidrar til underdiagnostisering – særlig blant jenter.</p>
+                    <figure>
+                        <img src="./src/pictures/ADHD/Tora.jpg" alt="Bilde av Tora" />
+                        <p className="photo-text">
+                        Dette er det første Tora Murtnes-Hatlestad (30) hadde å si da hun fikk spørsmål om hverdagen før hun fikk diagnosen. Hun fikk diagnosen i en alder av 29 år, og det var ingen som hadde mistenkt at hun kunne ha diagnosen før nå.
+                        </p>
+                        <figcaption className="photographer">
+                            Foto: Jenny Østreng
+                        </figcaption>
+                    </figure>
+
+                    <p>
+                        Statistikk viser en klar skjevfordeling mellom kjønnene: Mens 4,3 % av guttene i skolealder ble diagnostisert i perioden, gjaldt det samme kun 1,7 % av jentene. Denne ubalansen er ikke unik for Norge – internasjonal forskning peker på at gutters ADHD-symptomer ofte er mer synlige, preget av impulsivitet, hyperaktivitet og utagerende atferd. Jenter derimot kan ha mer subtile symptomer, noe som gjør dem lettere å overse.
+                    </p>
+
+                    <p>
+                        Her spiller lærere og foreldre en avgjørende rolle, ettersom de ofte står bak henvisningene til spesialister. Likevel viser en norsk undersøkelse at lærere har en tendens til å legge mer merke til gutters ADHD-symptomer enn jenters. Konsekvensen? Mange jenter får ikke diagnosen før langt senere i livet – eller aldri. Hva betyr dette for dem som vokser opp uten å bli sett?
+                    </p>
+                    <img src="" alt="" />
+
+                    <p>En nyere undersøkelse fra OsloMet, gjennomført for ADHD Norge i 2022, gir innsikt i utfordringene voksne med ADHD møter i helsevesenet. Av de 3085 voksne som deltok, svarte 58,6 % av kvinnene at de først fikk diagnosen etter fylte 30 år. For mange synes manglende eller dårlig henvisning til spesialister å være en viktig forklaringsfaktor. Blant de som oppga at de ikke fikk henvisning, fikk 44,8 % diagnosen etter fylte 30, og 25 % i alderen 20–30 år. </p>
+                    <img src="" alt="" />
+
+                    <p>En tilsvarende trend ble funnet blant de som rapporterte «dårlig henvisning» som utfordring: 48,4 % fikk diagnosen etter fylte 30, mens 23,2 % var i 20-årene. Dette peker på mulige svakheter i systemet som skal sikre tidlig identifisering og oppfølging, og en stor andel deltakere oppgir å ha et udekket behov for psykisk helsehjelp. Selv om årsakene kan være sammensatte, tyder funnene på et behov for å styrke henvisningspraksisen og tilgjengeligheten til spesialisthelsetjenester for voksne med ADHD. 
+                    </p>
+                    <img src="" alt="" />
+                </article>
+                <section>
                     <button id="Knapp" onClick={handleBtn}>Choose Story</button>
                     {randomChoise && (
                         <section>
@@ -31,30 +62,19 @@ export default function RandomArticle() {
                             </button>
                         </section>
                     )}
-                </article>
+                </section>
             </section>
             <section id="chart">
-                <ResponsiveContainer width="100%" height="100%">
-                    <LineChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                    >
+            <ResponsiveContainer width="100%" height={400}>
+                <BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis yAxisId="left" />
-                    <YAxis yAxisId="right" orientation="right" />
+                    <XAxis dataKey="age" label={{ value: "Alder (År)", position: "insideBottom", offset: -5 }} />
+                    <YAxis label={{ value: "Prosent (%)", angle: -90, position: "insideLeft" }} />
                     <Tooltip />
                     <Legend />
-                    <Line yAxisId="left" type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line yAxisId="right" type="monotone" dataKey="uv" stroke="#82ca9d" />
-                    </LineChart>
+                    <Bar dataKey="Jenter" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+                    <Bar dataKey="Gutter" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+                    </BarChart>
                 </ResponsiveContainer>
             </section>
         </main>
--- Website/src/components/Art.2/Toll.jsx
+++ Website/src/components/Art.2/Toll.jsx
@@ -24,11 +24,11 @@ export default function Toll({ setTollTitle, setTollQuote }) {
                     <article id="bom">
                         <section id="top">
                             <p className="sitat topQuote" id="tollQuote">-	De som styrer Sarpsborg vet ikke hvordan det er å sitte med tom lommebok, og istedenfor å se mennesker ser de tall, sier Gitte Yvonne Bjørndal Olsen (55) fra Sarpsborg.</p>
-                            <p>For mange i Sarpsborg og Fredrikstad har bompenger blitt en stor utgift i hverdagen. Vi har snakket med flere som må krysse bomringen daglig – til jobb, familie og fritid – og som føler bompengene påvirker både økonomi og livskvalitet.</p>
+                            <p>For mange i Sarpsborg og Fredrikstad har bompenger blitt en stor utgift i hverdagen. Flere innbyggere må krysse bomringen daglig – til jobb, familie og fritid – og føler at bompengene påvirker både økonomi og livskvalitet.</p>
                             
-                            <p className="sitat">-	Det er en regning som kommer på toppen av alt annet, som man egentlig aldri har råd til, sier Gitte Yvonne Bjørndal Olsen (55) fra Sarpsborg.</p>
-                            <h3></h3>
-                            <p>Hun kjører ofte omveier for å unngå bommene og planlegger turer til andre byer som Halden for å handle.</p>
+                            <p className="sitat">– Det er en regning som kommer på toppen av alt annet, som man egentlig aldri har råd til, sier Gitte Yvonne Bjørndal Olsen (55) fra Sarpsborg.</p>
+                            
+                            <p>Den uføre alenemoren forteller at hun ofte velger å kjøre omveier for å unngå bommene. I tillegg planlegger hun turer til andre byer, som Halden, hvor hun handler for å redusere utgiftene.</p>
                         </section>
                         {/* <p>Siden 20. mars 2024 har de 23 bomstasjonene i Sarpsborg og Fredrikstad krevd inn bompenger fra bilister. Hensikten er å finansiere Bypakke Nedre Glomma, et prosjekt for å forbedre infrastruktur, veier og kollektivtilbudet i området. Likevel opplever flere innbyggere, som Einar Kjell Nilsen (62), at bompengene og tilhørende prosjekter er unødvendige eller belastende.</p>
                         <p className="sitat">-	Bompengene er sure penger som ikke går til noen ting, sier Nilsen, som oppgir å passere bomringen omtrent ti ganger i uken, blant annet for å besøke familie i Fredrikstad.</p>
@@ -39,30 +39,43 @@ export default function Toll({ setTollTitle, setTollQuote }) {
                 </section>
                 <section className="textContent">              
                     <section className="textOverflow topText">
-                        <p id="topText" className="textOverlay">Siden 20. mars 2024 har de 23 bomstasjonene i Sarpsborg og Fredrikstad krevd inn bompenger fra bilister. Hensikten er å finansiere Bypakke Nedre Glomma, et prosjekt for å forbedre infrastruktur, veier og kollektivtilbudet i området. Likevel opplever flere innbyggere, som Einar Kjell Nilsen (62), at bompengene og tilhørende prosjekter er unødvendige eller belastende.</p>
+                        <p id="topText" className="textOverlay">Siden 20. mars 2024 har de 23 bomstasjonene i Sarpsborg og Fredrikstad krevd inn bompenger fra bilister. Hensikten er å finansiere Bypakke Nedre Glomma, et prosjekt som skal forbedre infrastrukturen, inkludert veier og kollektivtilbudet i området. Prosjektet skal gjøre det lettere å reise miljøvennlig i framtiden, men på kort sikt har det ført til frustrasjon blant mange bilister.</p>
 
-                        <p className="sitat textOverlay" id="sit">-	Bompengene er sure penger som ikke går til noen ting, sier Nilsen, som oppgir å passere bomringen omtrent ti ganger i uken, blant annet for å besøke familie i Fredrikstad.</p>
+                        <p className="textOverlay" id="sit">Einar Kjell Nilsen (62) fra Fredrikstad er blant dem som stiller spørsmål ved hele bompengeordningen:
+                            <p className="sitat" id="p-sit">– Bompengene er sure penger som ikke går til noen ting, sier han. Nilsen oppgir å passere bomringen omtrent ti ganger i uken for å besøke familie i Sarpsborg.</p>
+                        </p>
                     </section>
                 {/* </section> */}
 
                 
                 {/* <section className="textContent"> */}
                     <section className="textOverflow secondText">
-                        <p id="secondText" className="textOverlay">For en bilist med bensin- eller dieselbil uten bombrikke utgjør dette en månedlig kostnad på rundt 1280 kroner, noe som kan gjøre selv enkle ærender kostbare.</p>
+                        <p id="secondText" className="textOverlay">For en bilist med bensin- eller dieselbil uten bombrikke kan disse passeringene raskt bli en stor økonomisk byrde. Kostnaden kan komme opp i rundt 1280 kroner per måned, og for mange blir selv enkle ærender kostbare. For de som allerede har en stram økonomi, kan regningen bli uoverkommelig.</p>
 
-                        <p className="sitat textOverlay" id="sita">-	Regningen kommer hele tiden, og flere har gått til inkasso. Når man ikke klarer å betale, blir bombrikken stengt, og man får ikke lenger 20 % rabatt på passeringene. Da må man betale full pris, og det blir en slags ond sirkel, sier Gitte Yvonne Bjørndal Olsen.</p>
+                        <p className="sitat textOverlay" id="sita">– Regningen kommer hele tiden, og flere har gått til inkasso. Når man ikke klarer å betale, blir bombrikken stengt, og man mister 20 % rabatt på passeringene. Da må man betale full pris, og det blir en slags ond sirkel, sier Gitte Yvonne Bjørndal Olsen.</p>
                     </section>
                 </section>
                 <section id="map">
                     <Map/>
                 </section>
                 <section id="buttom">
-                    <p>Både Olsen og Nilsen påpeker at kostnadene ved bomringene særlig rammer de med lavere inntekt, som uføre og pensjonister, eller småbarnsfamilier som må kjøre barna til aktiviteter.</p>
+                    <article>
+                        <h3>Rammen skjevt</h3>
+                        <p>Flere innbyggere, som Olsen og Nilsen, opplever at bomringene rammer dem som har minst å rutte med, spesielt uføre, pensjonister og småbarnsfamilier. Mange av dem har ikke noe alternativ til bil.</p>
 
-                    <p className="sitat">-	De som styrer Sarpsborg vet ikke hvordan det er å sitte med tom lommebok, og istedenfor å se mennesker ser de tall, sier Olsen.</p>
-                    <p>Samtidig er det viktig å merke seg at inntektene fra bompengene skal brukes til å forbedre kollektivtransporten og utvikle veier i regionen, noe som kan gi langsiktige fordeler for innbyggerne ved at det blir enklere å sykle og gå. Det er et håp om at investeringene vil bidra til mindre biltrafikk og bedre miljøforhold i fremtiden (Bypakke Nedre Glomma).</p>
-                    
-                    <p className="sitat">-	Det er totalen som er viktig for Bypakke Nedre Glomma, det statlige nullvekstmålet og de kommende utbyggingsprosjektene, står det på deres nettside.</p>
+                        <p className="sitat">– De som styrer Sarpsborg vet ikke hvordan det er å sitte med tom lommebok. I stedet for å se mennesker, ser de bare tall, sier Olsen frustrert.</p>
+
+                        <h3>Innvesterer i familien</h3>
+                        <p>På den andre siden argumenterer forkjempere for bomringene at inntektene er nødvendige for å oppnå bærekraftige løsninger på lengre sikt. Bypakke Nedre Glomma bruker inntektene fra bompenger til å forbedre kollektivtransporten og utvikle infrastrukturen for syklister og fotgjengere.</p>
+                        
+                        <p className="sitat">– Det er totalen som er viktig for Bypakke Nedre Glomma, det statlige nullvekstmålet og de kommende utbyggingsprosjektene, står det på deres nettside.</p>
+
+                        <h3>Hva skjer videre?</h3>
+                        <p>Debatten om bomringer fortsetter, både i Sarpsborg og Fredrikstad, og i resten av landet. Mange innbyggere håper på bedre tilrettelegging for dem som er avhengige av bil, og som har dårligere økonomi, mens andre ser frem til effektene av en mer miljøvennlig transportpolitikk.</p>
+                        <p>For Gitte Yvonne Bjørndal Olsen og Einar Kjell Nilsen handler det imidlertid om dagens utfordringer – og hvordan man skal få endene til å møtes.</p>
+                        <p className="sitat">– Vi må få hverdagen til å gå rundt. Men det føles som om politikerne har glemt oss som faktisk må betale regningen, sier Olsen.</p>
+                        <p>Uansett hvilken side av saken man står på, er det klart at bomringene har blitt et sentralt tema i byenes liv og politikk. Spørsmålet blir om innbyggerne vil føle at pengene de betaler, til slutt er verdt det, eller ikke.</p>
+                    </article>
                 </section>
             </main>
         </>
